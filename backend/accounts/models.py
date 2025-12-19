7080a5006899cef1a5dd4595b281aa700dddae70
eac-xii <eacxii1870@naver.com>
2025-12-19 16:50:31 +0900

    feat: add UserFeatures to accounts models

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
+from musics.models import Track
+from django.core.validators import (
+    MinValueValidator,
+    MaxValueValidator
+)
+
+float_scope_validator = [
+    MinValueValidator(0.0),
+    MaxValueValidator(1.0)
+]
 
 class UserManager(BaseUserManager):
     def create_user(self, email, username, last_name, first_name, nickname, password=None):
@@ -148,3 +158,57 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"{self.user.nickname}'s profile"
+    
+class UserFeatures(models.Model):
+    track = models.OneToOneField(
+        Track,
+        on_delete=models.CASCADE,
+        related_name="audio_features"
+    )
+    acousticness = models.FloatField(
+        validators=float_scope_validator
+    )
+    danceability = models.FloatField(
+        validators=float_scope_validator
+    )
+    energy = models.FloatField(
+        validators=float_scope_validator
+    )
+    instrumentalness = models.FloatField(
+        validators=float_scope_validator
+    )
+    KEY_CHOICES = [
+        (-1, "unknown"),
+        (0, "C"), (1, "C♯/D♭"), (2, "D"), (3, "D♯/E♭"),
+        (4, "E"), (5, "F"), (6, "F♯/G♭"), (7, "G"),
+        (8, "G♯/A♭"), (9, "A"), (10, "A♯/B♭"), (11, "B")
+    ]
+    key = models.SmallIntegerField(
+        choices=KEY_CHOICES,
+        null=True,
+    )
+    liveness = models.FloatField(
+        validators=float_scope_validator
+    )
+    loudness = models.FloatField(
+        validators=[
+            MinValueValidator(-60.0),
+            MaxValueValidator(0.0)
+        ]
+    )
+    mode = models.BooleanField(
+        null=True,
+        help_text="True=major, False=minor"
+    )
+    speechiness = models.FloatField(
+        validators=float_scope_validator
+    )
+    tempo = models.FloatField(
+        validators=[
+            MinValueValidator(0.0),
+            MaxValueValidator(300.0)
+        ]
+    )
+    valence = models.FloatField(
+        validators=float_scope_validator
+    )
