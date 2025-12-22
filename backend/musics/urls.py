5d414b067aa6321e5aa0848eb040f9738c209a27
eac-xii <eacxii1870@naver.com>
2025-12-22 13:22:32 +0900

    feat: implement hybrid search with similarity-based ranking

--- backend/musics/urls.py
+++ backend/musics/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('spotify/playback_token/', views.spotify_playback_token),
     path('spotify/transfer_playback/', views.spotify_transfer_playback),
     path('spotify/play/', views.spotify_play),
+    path('spotify/search/', views.spotify_search),
 ]
